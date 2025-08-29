621bea1dfed7c0203dadbfdf563bb9446c3920a6
NoahButCool <noah.nde9@gmail.com>
2025-08-29 11:24:45 -0500

    added mark task complete

--- todo.py
+++ todo.py
@@ -2,6 +2,7 @@
 
 # Step 1: Start with an empty list to hold tasks
 tasks = []
+completed_tasks = []
 
 # Step 2: Add a task
 def add_task(task):
@@ -18,6 +19,10 @@ def delete_task(index):
     print(f"Deleted {task}")
 
 # Step 5: Mark task complete
+def mark_complete(index):
+    task = tasks.pop(index)
+    completed_tasks.append(task)
+    print(f"{task} marked complete")
 
 
 # Step 6: Save/load tasks (extra stretch for today)
@@ -28,8 +33,6 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-    delete_task(0)
+    mark_complete(0)
     view_tasks()
-    # mark_complete(0)
-    # view_tasks()
     # save_tasks()
